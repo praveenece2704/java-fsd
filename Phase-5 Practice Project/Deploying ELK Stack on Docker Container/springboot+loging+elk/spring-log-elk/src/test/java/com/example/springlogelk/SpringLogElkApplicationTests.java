c1dac65e456abd73ce4d6b4d2cc0f68c89883c52
praveenece2704 <73685739+praveenece2704@users.noreply.github.com>
2022-10-17 11:56:21 +0530

    Docker with ELK

---
+++ Phase-5 Practice Project/Deploying ELK Stack on Docker Container/springboot+loging+elk/spring-log-elk/src/test/java/com/example/springlogelk/SpringLogElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlogelk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
